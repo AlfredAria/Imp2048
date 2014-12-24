e7f432f35169f8957bc0f198a568c9ad53e11203
Alfred.Huang <alfred2001@live.cn>
2014-12-24 11:03:37 +0800

    Improving UI

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,29 +1,55 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 
 public class GamePanel extends JPanel implements KeyListener {
 
-	protected final int boardSize = 3;
+	// Game logic components
+	protected final int boardSize = 4;
+	protected int[][]   boardData = new int[boardSize][boardSize];
 	protected GameLogic logic = new GameLogic(boardSize);
-	protected int[][] boardData = new int[boardSize][boardSize];
-	protected boolean gameLost = false;
-	protected boolean gameWon = false;
 	
-	public GamePanel() {
+	// UI components
+	protected JFrame parent;
+	protected final int tileWidth  = 65;
+	protected final int tileHeight = 65;
+	protected final int tileMargin = 5; 
+	protected final Font fontSize   = new Font("Arial", Font.BOLD, 9);
+	public final int panelWidth = boardSize * (tileWidth + tileMargin) + tileMargin;
+	public final int panelHeight= boardSize * (tileHeight+ tileMargin) + tileMargin;
+//	protected JLabel label = new JLabel("");
+	
+	public GamePanel(JFrame parent) {
+		super(new BorderLayout());
+//		label.setSize(panelWidth, panelHeight);
+//		this.add(label, BorderLayout.CENTER);
+		this.parent = parent;
 		this.setOpaque(true);
 		this.setVisible(true);
+		this.setBounds(0,0,panelHeight, panelWidth);
 		boardData = logic.getBoard();
 		this.repaint();
 	}
 	
 	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+		if (logic.getLose()) {
+			JOptionPane.showMessageDialog(null, "You lost.", 
+					"Message", JOptionPane.WARNING_MESSAGE, new ImageIcon("C:\\Users\\01420819\\workspace\\Imp2048\\fail.png"));
+			parent.removeKeyListener(this);
+		}
+		if (logic.getWon()) JOptionPane.showMessageDialog(null, "You won.",
+					"Message", JOptionPane.WARNING_MESSAGE, new ImageIcon("C:\\Users\\01420819\\workspace\\Imp2048\\tick.jpg"));
 		switch(arg0.getKeyCode()) {
 		case KeyEvent.VK_LEFT:  
 			boardData = logic.moveLeft();
@@ -43,9 +69,19 @@ public class GamePanel extends JPanel implements KeyListener {
 	
 	public void paint(Graphics g) {
 		super.paint(g);
-		BlockRotate.print(boardData);
-		if (logic.getLose()) System.out.println("Lost.");
-		if (logic.getWon()) System.out.println("Won.");
+		for (int y = 0; y < boardSize; y ++) {
+			for (int x = 0; x < boardSize; x ++) {
+				g.setColor(Color.BLUE);
+				int dx = (x+1)*tileMargin + x*tileWidth;
+				int dy = (y+1)*tileMargin + y*tileHeight;
+				g.fillRect(dx, dy, tileWidth, tileHeight);
+				if (boardData[y][x] != 0) {
+					g.setFont(fontSize);
+					g.setColor(Color.WHITE);
+					g.drawString(boardData[y][x] + "", dx + tileWidth*2/3, dy + tileHeight*2/3);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -64,11 +100,11 @@ public class GamePanel extends JPanel implements KeyListener {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				JFrame frame = new JFrame();
-				GamePanel panel = new GamePanel(); 
-				frame.setSize(400, 400);
+				GamePanel panel = new GamePanel(frame); 
 				frame.addKeyListener(panel);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setContentPane(panel);
+				frame.setBounds(0, 0, panel.panelWidth, panel.panelHeight);
 				frame.setVisible(true);
 			}
 		});
